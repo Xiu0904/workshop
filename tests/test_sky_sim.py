a8c684d736a96020c09d617dd64c5fa42e56ad1e
Xiu0904 <xiuliu0904@gmail.com>
2023-09-06 08:25:06 +0000

    project after workshop day 111

---
+++ tests/test_sky_sim.py
@@ -0,0 +1,15 @@
+def test_module_import():
+    try:
+        from mymodule import sky_sim
+    except Exception as e:
+        raise AssertionError("Failed to import mymodule")
+    return
+def test_get_radec():
+    from mymodule import sky_sim
+    
+    expected = (14.215420962967535, 41.26916666666667)
+    answer = sky_sim.get_radec()
+    
+    if not( (expected[0]==answer[0]) and (expected[1]==answer[1]) ):
+        raise AssertionError(f"get_radec() gives {answer} instead of {expected}")
+    return
